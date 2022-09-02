122ad6dc81a78b95b121fb2774de92bc6ea4b012
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-02 05:19:26 -0700

    completed temp conversion challenge

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let celcius = Math.ceil(((temperature - 32) * (5/9)) * 10);
+  return celcius / 10
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let farenheit = Math.ceil((temperature * (9/5) + 32) * 10);
+  return farenheit / 10
 };
 
 // Do not edit below this line
