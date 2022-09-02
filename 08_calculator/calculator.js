ddfe1769be9745a9f4bde615e83abbf771af9883
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-02 05:36:51 -0700

    completed calculator challenge problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let summed = 0;
+	for (let i = 0; i < array.length; i++) {
+    summed += array[i];
+  }
+  return summed;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let postMultiplied = 1
 
+  for (let i = 0; i < numbers.length; i++) {
+    postMultiplied *= numbers[i];
+  };
+  return postMultiplied;
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	return number1 ** number2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	factorized = 1;
+
+  for (let i = 1; i <= number; i++) {
+    factorized *= i;
+  };
+  return factorized
 };
 
 // Do not edit below this line
