4ef15601d403061814f577c92b9cadd7168a6575
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-01 23:08:29 -0700

    passed first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, freq) {
+  let echoString = string
+  for (let i = 1; i < freq; i++) {
+    echoString += string;
+  };
+  return echoString
 };
 
 // Do not edit below this line
