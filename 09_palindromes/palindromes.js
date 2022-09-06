bd2c9263798861f29f5d65651c7b228f4aebc3d7
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-06 15:53:31 -0700

    completed palindrome challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  word = word.toLowerCase();
+  word = word.replace(/\s/g, '');
+  let i = 0;
+  let j = word.length - 1;
 
+  while (i <= j) {
+    console.log(word[i], word[j])
+    if (/[^a-zA-Z]/.test(word[i])) {
+      i++;;
+    } else if (/[^a-zA-Z]/.test(word[j])) {
+      j--;
+    }
+
+    if (word[i] !== word[j]) {
+      return false;
+    };
+    i++;
+    j--;
+  }
+  return true
 };
 
 // Do not edit below this line
