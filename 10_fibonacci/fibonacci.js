2caccd457caa648285d535e36c12f66523ea27af
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-06 20:51:14 -0700

    completed fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
-};
+  if (num <= 0) return 'OOPS'
+  let array = [1];
+  let j = 1
+
+  for (let i = 1; i <= num -1; i++) {
+    if (i === 1) {
+      array.push(i);;
+    } else {
+      array.push(array[i-1] + array[i-2])
+    }
+  };
+  return array.pop()
+}
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
