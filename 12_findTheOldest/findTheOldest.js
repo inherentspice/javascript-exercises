6994aed701a7dfc509712a46e10b83e2a73c9876
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-06 21:15:05 -0700

    completed findtheOldest challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let currentTime = new Date().getFullYear();
+  const oldestSort = people.sort(function (personOne, personTwo) {
+    if (!personOne.yearOfDeath) {
+      personOne.yearOfDeath = currentTime;
+    }
+    if (!personTwo.yearOfDeath) {
+      personTwo.yearOfDeath = currentTime;
+    }
+    let personOneAge = personOne.yearOfDeath - personOne.yearOfBirth;
+    let personTwoAge = personTwo.yearOfDeath - personTwo.yearOfBirth;
+    return personOneAge > personTwoAge ? 1: -1
+  });
+  console.log(oldestSort)
+  return oldestSort.pop();
 };
 
 // Do not edit below this line
